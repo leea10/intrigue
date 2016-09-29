beabf2b6680018f2e776f2cca29378a2cf051485
Ariel Lee <leea10@rpi.edu>
2016-09-29 13:36:54 -0400

    Updated gitignore

---
+++ app/controllers/api.server.controller.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose");
+var User = require("./../schema.js").User;
+var Story = require("./../schema.js").Story;
+var Character = require("./../schema.js").Character;
+var Snapshot = require("./../schema.js").Snapshot;
+var Relationship = require("./../schema.js").Relationship;
+var Node = require("./../schema.js").Node;
+var Tag = require("./../schema.js").Tag;
+
+
+/**
+ *
+ * @param {object} req
+ *   The express HTTP request containing the information required for the function
+ * @param {object} res
+ *   The express HTTP response to be sent back to the requester
+ */
+exports.saveStory = function(req, res) {
+};
+
+/**
+ *
+ * @param {object} req
+ *   The express HTTP request containing the information required for the function
+ * @param {object} res
+ *   The express HTTP response to be sent back to the requester
+ */
+exports.removeStory = function(req, res) {
+
+};
