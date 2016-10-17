81ec29d68f690239e7fd841ce854cf50922ada99
Ariel Lee <ariellee616@gmail.com>
2016-10-17 19:15:58 -0400

    Editor (#14)
    Drag and drop works with left click
    Right click to select (context menu?)
    Temporary: Create node with double click for now

--- app/client/editor_canvas.js
+++ app/client/editor_canvas.js
@@ -1,12 +1,31 @@
+/**
+ * @fileoverview Class solely responsible for drawing onto the editor canvas.
+ */
 /*export*/ class EditorCanvas {
-    constructor(ctx, sInc, bInc) {
-        this.canvas_ = ctx;
+    /**
+     * @param domElement The element in the DOM that the editor should be associated with
+     * @param sInc distance between lines on the fine grid.
+     * @param bInc distance between lines on the main grid.
+     * @constructor
+     */
+    constructor(domElement, sInc, bInc) {
+        this.domElement_ = domElement;
+        this.canvas_ = domElement.getContext('2d');
         this.smallInc_ = sInc; // Number of pixels between each thin grid line.
         this.bigInc_ = bInc;   // Number of pixels between each thick grid line.
-        this.nodes_ = [];
+        this.nodes_ = []; // Nodes to draw (in order)
     }
 
     // public methods
+    /**
+     * Changes the editor canvas's dimensions to (width, height).
+     * @param width in pixels.
+     * @param height in pixels.
+     */
+    changeSize(width, height) {
+        this.domElement_.width = width;
+        this.domElement_.height = height;
+    }
 
     draw() {
         // Clear the background
@@ -15,13 +34,26 @@
         this.drawGrid_();
         // Draw the nodes
         for(let i = 0; i < this.nodes_.length; i++) {
-            let node = this.nodes_[i];
-            this.drawCircle_(node.x, node.y, node.r);
+            this.nodes_[i].draw(this.canvas_);
         }
     }
 
     width() {   return this.canvas_.canvas.width;   }
     height() {  return this.canvas_.canvas.height;  }
+
+    /**
+     * @param x
+     * @param y
+     * @returns {Node} The top node that intersects the point (x,y)
+     */
+    getNodeAtPoint(x, y) {
+        for(let i = this.nodes_.length - 1; i >= 0; i--) {
+            if(this.nodes_[i].intersectsPoint(x, y)) {
+                return this.nodes_[i];
+            }
+        }
+        return null;
+    }
 
     /**
      * Adds a node of radius r at absolute position (x, y).
@@ -30,11 +62,7 @@
      * @param r
      */
     addNode(x, y, r) {
-        this.nodes_.push({
-            x: x,
-            y: y,
-            r: r
-        });
+        this.nodes_.push(new Node(x, y, r));
     }
 
     /**
@@ -69,23 +97,64 @@
         this.canvas_.lineTo(p2.x, p2.y);
         this.canvas_.stroke();
     }
+}
 
-    /**
-     * Draws a node of radius at position x, y on the canvas.
-     * @param x
-     * @param y
-     * @param radius
-     */
-    drawCircle_(x, y, radius) {
+/* export */ class Node {
+    constructor(x, y, radius) {
+        this.x_ = x;
+        this.y_ = y;
+        this.radius_ = radius;
+        this.strokeColor_ = '#8fd3d2';
+        this.strokeWeight_ = 4;
+    }
+
+    draw(ctx) {
         // Set properties
-        this.canvas_.fillStyle = '#fefefe';
-        this.canvas_.strokeStyle = '#8fd3d2';
-        this.canvas_.lineWidth = 4;
+        ctx.fillStyle = '#fefefe';
+        ctx.strokeStyle = this.strokeColor_;
+        ctx.lineWidth = this.strokeWeight_;
 
         // Draw the circle
-        this.canvas_.beginPath();
-        this.canvas_.arc(x, y, radius, 0, 2*Math.PI, false);
-        this.canvas_.fill();
-        this.canvas_.stroke();
+        ctx.beginPath();
+        ctx.arc(this.x_, this.y_, this.radius_, 0, 2*Math.PI, false);
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    /**
+     * Dispplaces the node
+     * @param x Number of pixels to displace horizontally
+     * @param y Number of pixles to displace vertically
+     */
+    move(x, y) {
+        this.x_ += x;
+        this.y_ += y;
+    }
+
+    /**
+     * Checks if point (x, y) lies inside the node
+     * @param x
+     * @param y
+     * @returns {boolean}
+     */
+    intersectsPoint(x, y) {
+        // Distance check
+        return Math.sqrt(Math.pow(x-this.x_, 2) + Math.pow(y-this.y_, 2)) <= this.radius_;
+    }
+
+    /**
+     * Changes the node to draw in a selected state.
+     */
+    select() {
+        this.strokeColor_ = 'blue';
+        this.strokeWeight_ = 6;
+    }
+
+    /**
+     * Changes the node to draw in a normal state.
+     */
+    deselect() {
+        this.strokeColor_ = '#8fd3d2';
+        this.strokeWeight_ = 4;
     }
 }
