e2279e38f3c45be1ff101fc1d0764542de3c6ca2
Ariel Lee <leea10@rpi.edu>
2016-10-24 12:24:56 -0400

    Small cleany up things

--- app/client/editor_canvas.js
+++ app/client/editor_canvas.js
@@ -14,6 +14,7 @@ class EditorCanvas {
         this.smallInc_ = sInc; // Number of pixels between each thin grid line.
         this.bigInc_ = bInc;   // Number of pixels between each thick grid line.
         this.nodes_ = []; // Nodes to draw (in order)
+        this.nodeIndex_ = {};
     }
 
     // public methods
