164ceb8a7772e9c1744754a3482908b93f280283
Ezra Dowd <ezra.dowd@gmail.com>
2016-10-24 13:20:24 -0400

    Ezras magical land of wonderous things (#19)
    I did the thing

---
+++ app/client/dashboard.controller.js
@@ -0,0 +1,29 @@
+app.controller('DashboardController', function($scope, $http){
+    $scope.stories = [];
+    $scope.hideForm = true;
+
+    $scope.addStory = () => {
+        $http.post('/saveStory',{
+            title : $scope.title,
+            description : $scope.description,
+            image: $scope.image
+        }).success(function (obj){
+            $scope.stories.push(obj);
+            console.log(obj);
+        }).error(function (err){
+            console.log(err);
+        });
+
+    };
+
+    $scope.showForm = () => {
+        $scope.hideForm = !$scope.hideForm;
+    };
+
+    $http.get('/getStories').then((res) => {
+        $scope.stories = res.data.data;
+    }, (res) => {
+        console.log(res)
+    });
+});
+
