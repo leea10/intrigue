291f612150a970bc1cc404a9654c06c8712a232a
Ezra Dowd <ezra.dowd@gmail.com>
2016-10-30 14:49:25 -0400

    Ezras magical land of wonderous things (#27)
    Added functionality for uploading images, probably

--- express_config/express.js
+++ express_config/express.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 module.exports = () => {
     const app = express();
 
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
     app.use(require('express-ejs-layouts'));
+
 
     const session = require('express-session');
     const MongoStore = require('connect-mongo')(session);
@@ -28,6 +30,8 @@ module.exports = () => {
 
     const morgan = require('morgan');
     app.use(morgan('dev'));
+
+    app.use(fileUpload());
 
     require('../app/db');
     require('../app/routes/index.server.routes')(app);
