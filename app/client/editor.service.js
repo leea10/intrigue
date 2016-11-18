b5453cdff491b994256260962bad905963154ce7
Ariel Lee <ariellee616@gmail.com>
2016-11-18 15:01:49 -0500

    Polished up Editor Service API so it no longer knows about the editor's current snapshot

--- app/client/editor.service.js
+++ app/client/editor.service.js
@@ -1,44 +1,102 @@
 app.service('EditorService', function($http, $location) {
     this.storyId_ = $location.search().id;
-    this.storyDetails_ = null;
-    this.currentSnapshot_ = null;
-    this.characterLookup_ = null;
+    this.storyDetails_ = {};
+    this.characterLookup_ = {};
+    this.snapshotLookup_ = {};
+    this.nodeLookup_ = {};
 
     // Initialization code
     this.initPromise_ = $http.get('/api/story/detail?storyID=' + this.storyId_).then((response) => {
-        // Populate story details
+        // Populate story details.
         this.storyDetails_ = response.data.data;
-        this.characterLookup_ = {};
-        this.nodeLookup_ = {};
-        for(let i = 0; i < this.storyDetails_.characters.length; i++){
-            this.characterLookup_[this.storyDetails_.characters[i]._id] = this.storyDetails_.characters[i];
+        // Populate character lookup.
+        let characters = this.storyDetails_.characters;
+        for (let i = 0; i < characters.length; i++) {
+            let character = characters[i];
+            this.characterLookup_[character._id] = character;
         }
-        this.currentSnapshot_ = this.storyDetails_.snapshots[0];
-        console.log('Loading snapshot ' + this.currentSnapshot_._id);
+        // Populate snapshot lookup.
+        let snapshots = this.storyDetails_.snapshots;
+        for (let i = 0; i < snapshots.length; i++) {
+            let snapshot = snapshots[i];
+            this.snapshotLookup_[snapshot._id] = snapshot;
+        }
     });
 
+    /**
+     * @returns {Promise} A promise that this editor service will initialize.
+     */
     this.init = () => {
         return this.initPromise_;
     };
 
-    this.getCharacter = (id) => {
-        let character = this.characterLookup_[id];
-        if(character !== undefined) {
-            return character;
+    /**
+     * @param index
+     * @returns {*} Snapshot object at given index.
+     */
+    this.getSnapshotAt = (index) => {
+        return this.storyDetails_.snapshots[index];
+    };
+
+    /**
+     * @param id
+     * @returns {*} Snapshot object with the given id.
+     */
+    this.getSnapshot = (id) => {
+        // Checks the snapshot lookup.
+        let snapshot = this.snapshotLookup_[id];
+        if(snapshot !== undefined) {
+            return snapshot;
         }
-        for(let i = 0; i < this.storyDetails_.characters.length; i++){
-            if(this.storyDetails_.characters[i]._id === id){
-                this.characterLookup_[id] = this.storyDetails_.characters[i];
-                return this.storyDetails_.characters[i];
+        // If the lookup misses, search through the snapshot array in story details.
+        let snapshots = this.storyDetails_.snapshots;
+        for(let i = 0; i < snapshots.length; i++){
+            let snapshot = snapshots[i];
+            if(snapshot._id === id){
+                // Add snapshot to the lookup.
+                this.snapshotLookup_[id] = snapshot;
+                return snapshot;
             }
         }
+        // Snapshot with given id does not exist in this story.
         return null;
     };
 
+    /**
+     * @returns {Array|*} An array of all the character objects in this story.
+     */
     this.getCharacters = () => {
         return this.storyDetails_.characters;
     };
 
+    /**
+     * @param id
+     * @returns {*} Character object with given id.
+     */
+    this.getCharacter = (id) => {
+        // Checks the character lookup.
+        let character = this.characterLookup_[id];
+        if(character !== undefined) {
+            return character;
+        }
+        // If the lookup misses, search through the character array in story details.
+        let characters = this.storyDetails_.characters;
+        for(let i = 0; i < characters.length; i++){
+            let character = characters[i];
+            if(character._id === id){
+                // Add character to the lookup.
+                this.characterLookup_[id] = character;
+                return character;
+            }
+        }
+        // Character with given id does not exist in this story.
+        return null;
+    };
+
+    /**
+     * @param characterObj Character to add to the story.
+     * @returns {Promise}
+     */
     this.addCharacter = (characterObj) => {
         let fData = new FormData();
         for(let property in characterObj) {
@@ -59,31 +117,51 @@ app.service('EditorService', function($http, $location) {
         });
     };
 
+    this.getNodes = (snapshotID) => {
+        return this.getSnapshot[snapshotID].nodes;
+    };
+
     /**
-     * Gets the node in the current snapshot with the given id.
+     * @param nodeID
+     * @param snapshotID [OPTIONAL] heuristic - snapshot that the node might belong to
+     * @returns {*} node object with the given node ID.
      */
-    this.getNode = (id) => {
-        let node = this.nodeLookup_[id];
-        if(node !== undefined) {
+    this.getNode = (nodeID, snapshotID) => {
+        // First, check the node index.
+        let node = this.nodeLookup_[nodeID];
+        if (node !== undefined) {
             return node;
         }
-        let nodes = this.currentSnapshot_.nodes;
-        for(let i = 0; i < nodes.length; i++){
-            let node = nodes[i];
-            if(node._id === id){
-                this.nodeLookup_[id] = node;
-                return node;
+        // If a snapshotID was provided, restrict the search to the provided snapshot.
+        if (snapshotID !== undefined) {
+            let nodes = this.getSnapshot[snapshotID].nodes;
+            for (let i = 0; i < nodes.length; i++) {
+                let node = nodes[i];
+                if (node._id === nodeID) {
+                    this.nodeLookup_[nodeID] = node;
+                    return node;
+                }
             }
         }
+        // Last resort, search the rest of the snapshots.
+        let snapshots = this.storyDetails_.snapshots;
+        for (let i = 0; i < snapshots.length; i++) {
+            let snapshot = snapshots[i];
+            // Skipped the snapshot we already checked
+            if(snapshot._id === snapshotID) {
+                continue;
+            }
+            let nodes = snapshot.nodes;
+            for (let j = 0; j < nodes.length; j++) {
+                let node = nodes[i];
+                if (node._id === nodeID) {
+                    this.nodeLookup_[nodeID] = node;
+                    return node;
+                }
+            }
+        }
+        // Node with given id does not exist in this story.
         return null;
-    };
-
-    this.getNodes = () => {
-        return this.currentSnapshot_.nodes;
-    };
-
-    this.getRelationships = () => {
-        return this.currentSnapshot_.relationships;
     };
 
     this.addNode = (x, y, characterID) => {
@@ -100,6 +178,10 @@ app.service('EditorService', function($http, $location) {
             this.nodeLookup_[newNode._id] = newNode;
             return newNode;
         });
+    };
+
+    this.getRelationships = (snapshotID) => {
+        return this.getSnapshot[snapshotID].relationships_;
     };
 
     /**
