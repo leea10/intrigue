4328fb21dce8b2d533d8d2b37f95265d86aea995
Ariel Lee <ariellee616@gmail.com>
2016-11-19 12:21:43 -0500

    Make the editor canvas controller hold some data

--- app/client/editor_canvas.controller.js
+++ app/client/editor_canvas.controller.js
@@ -1,3 +1,13 @@
 app.controller('EditorCanvasController', function($scope, EditorService) {
+    this.currentSnapshot_ = null;
+    $scope.nodes = [];
+    $scope.nodeIndex_ = {};
+    $scope.relationships = [];
 
+    // Initialization
+    EditorService.init().then(() => {
+        this.currentSnapshot_ = EditorService.getFirstSnapshotID(); // TEMPORARY HACK.
+        $scope.nodes = EditorService.getNodes(this.currentSnapshot_);
+        $scope.relationships = EditorService.getRelationships(this.currentSnapshot_);
+    });
 });
