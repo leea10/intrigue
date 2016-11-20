4918b5f1d2470a912f62eae2feb2d0db703cbacf
Ariel Lee <ariellee616@gmail.com>
2016-11-20 16:27:16 -0500

    Separated add/edit form scopes. Added error handling for no name field input

--- app/client/toolbar.controller.js
+++ app/client/toolbar.controller.js
@@ -18,6 +18,11 @@ app.controller('ToolbarController', ($scope, EditorService) => {
     $scope.onClose = () => {
         $scope.activeTool = null;
         $scope.character = null;
+        $scope.newCharacter = null;
+        $scope.image = null;
+        $scope.newImage = null;
+        $scope.errorMsg = null;
+        $scope.newErrorMsg = null;
         $scope.$broadcast('formClose');
     };
 
@@ -27,15 +32,15 @@ app.controller('ToolbarController', ($scope, EditorService) => {
 
     $scope.addCharacter = () => {
         $scope.errorMsg = null;
-        if(!$scope.character.name) {
-            $scope.errorMsg = 'Please enter a character name';
+        if(!$scope.newCharacter || !scope.newCharacter.name) {
+            $scope.newErrorMsg = 'Please enter a character name';
         }
-        if(!$scope.errorMsg){
-            if($scope.image) {
-                $scope.character.image = $scope.image[0];
+        if(!$scope.newErrorMsg){
+            if($scope.newImage) {
+                $scope.newCharacter.image = $scope.newImage[0];
             }
-            EditorService.addCharacter($scope.character).then(() => {
-                $scope.character = null;
+            EditorService.addCharacter($scope.newCharacter).then(() => {
+                $scope.newCharacter = null;
                 $scope.setActiveTool(null);
                 $scope.$broadcast('formSubmit');
             });
@@ -57,7 +62,7 @@ app.controller('ToolbarController', ($scope, EditorService) => {
 
     $scope.saveCharacter = () => {
         $scope.errorMsg = null;
-        if(!$scope.character.name) {
+        if(!$scope.character || !$scope.character.name) {
             $scope.errorMsg = 'Please enter a character name';
         }
         if(!$scope.errorMsg){
