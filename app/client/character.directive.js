e78c16fd475a0a5430fc0a4e3558b6d6a308682e
Ariel Lee <ariellee616@gmail.com>
2016-11-20 22:59:23 -0500

    a derpy thing shows up when hovering over character

--- app/client/character.directive.js
+++ app/client/character.directive.js
@@ -3,6 +3,17 @@ app.directive('character', ($filter) => {
         restrict: 'A',
         link: (scope, element, attributes) => {
             let base_url = 'images/characters/' + scope.character._id + '.' + scope.character.img_extension;
+
+            element.on('mouseenter', () => {
+                scope.hovered = true;
+                scope.$digest();
+            });
+
+            element.on('mouseleave', () => {
+                scope.hovered = false;
+                scope.$digest();
+            });
+
             scope.$on('editCharacterSuccessful', (event, data) => {
                 if(data.id === scope.character._id && data.changedImage) {
                     reloadImage();
